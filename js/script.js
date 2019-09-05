55ef99d03bac62c8ba71fdf132c64ae69d8b6907
Fabricio Parola <fabricio.parola@gmail.com>
2019-09-05 10:18:14 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const helloWorld = 
+`
+.o88b.  .d88b.  d8888b. d88888b   db    db d8888b.
+d8P  Y8 .8P  Y8. 88   8D 88'        8b  d8' 88  8D
+8P      88    88 88   88 88ooooo     8bd8'  88oodD'
+8b      88    88 88   88 88         .dPYb.  88  
+Y8b  d8  8b  d8' 88  .8D 88.       .8P  Y8. 88     
+  Y88P'   Y88P'  Y8888D' Y88888P   YP    YP 88    
+`; 
+
+
+console.log(helloWorld); 
