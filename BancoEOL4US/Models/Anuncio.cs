79a94590f150bab0613bb5657837beaf30596839
lucas alves <lucas.a.pereira05@gmail.com>
2019-10-29 09:31:37 -0300

    adicionando base do banco

---
+++ BancoEOL4US/Models/Anuncio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BancoEOL4US.Models
+{
+    public partial class Anuncio
+    {
+        public Anuncio()
+        {
+            Interesse = new HashSet<Interesse>();
+        }
+
+        [Key]
+        [Column("idAnuncio")]
+        public int IdAnuncio { get; set; }
+        [Column("precoAnuncio", TypeName = "decimal(7, 2)")]
+        public decimal PrecoAnuncio { get; set; }
+        [Column("dt_finalAnuncio", TypeName = "date")]
+        public DateTime DtFinalAnuncio { get; set; }
+        [Required]
+        [Column("fotoAnuncio")]
+        [StringLength(100)]
+        public string FotoAnuncio { get; set; }
+        [Required]
+        [Column("statusAnuncio")]
+        public bool? StatusAnuncio { get; set; }
+        [Column("FK_idProduto")]
+        public int? FkIdProduto { get; set; }
+        [Column("FK_idCondicao")]
+        public int? FkIdCondicao { get; set; }
+
+        [ForeignKey(nameof(FkIdCondicao))]
+        [InverseProperty(nameof(Condicao.Anuncio))]
+        public virtual Condicao FkIdCondicaoNavigation { get; set; }
+        [ForeignKey(nameof(FkIdProduto))]
+        [InverseProperty(nameof(Produto.Anuncio))]
+        public virtual Produto FkIdProdutoNavigation { get; set; }
+        [InverseProperty("FkIdAnuncioNavigation")]
+        public virtual ICollection<Interesse> Interesse { get; set; }
+    }
+}
---
+++ BancoEOL4US/Models/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BancoEOL4US.Models
+{
+    public partial class Produto
+    {
+        public Produto()
+        {
+            Anuncio = new HashSet<Anuncio>();
+        }
+
+        [Key]
+        [Column("idProduto")]
+        public int IdProduto { get; set; }
+        [Required]
+        [Column("nomeProduto")]
+        [StringLength(50)]
+        public string NomeProduto { get; set; }
+        [Required]
+        [Column("fabricanteProduto")]
+        [StringLength(50)]
+        public string FabricanteProduto { get; set; }
+        [Column("dt_lancProduto", TypeName = "date")]
+        public DateTime DtLancProduto { get; set; }
+        [Required]
+        [Column("descricaoProduto", TypeName = "text")]
+        public string DescricaoProduto { get; set; }
+        [Column("FK_idCategoria")]
+        public int? FkIdCategoria { get; set; }
+        [Column("FK_idUsuario")]
+        public int? FkIdUsuario { get; set; }
+
+        [ForeignKey(nameof(FkIdCategoria))]
+        [InverseProperty(nameof(Categoria.Produto))]
+        public virtual Categoria FkIdCategoriaNavigation { get; set; }
+        [ForeignKey(nameof(FkIdUsuario))]
+        [InverseProperty(nameof(Usuario.Produto))]
+        public virtual Usuario FkIdUsuarioNavigation { get; set; }
+        [InverseProperty("FkIdProdutoNavigation")]
+        public virtual ICollection<Anuncio> Anuncio { get; set; }
+    }
+}
