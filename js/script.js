21795d025e267b8d2331418c6f85a5c3144f7f76
lucas alves <lucas.a.pereira05@gmail.com>
2019-11-12 10:27:58 -0300

    adicionando telas de dúvida e como funciona

--- js/script.js
+++ js/script.js
@@ -11,15 +11,35 @@ Y8b  d8  8b  d8' 88  .8D 88.       .8P  Y8. 88
 
 console.log(helloWorld); 
 
+// início código tela de dúvidas - acordeon
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("ative");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+// final código tela de dúvidas - acordeon
+
+// início código tela de como funciona - carrossel
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -39,3 +59,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+// final código tela de como funciona - carrossel
